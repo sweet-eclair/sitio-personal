1321c9bd4ce7eec5faf62563fb8cb00ad294a112
sweet-eclair <75551469+sweet-eclair@users.noreply.github.com>
2023-01-23 00:08:05 -0500

    debug

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,20 +1,3 @@
-/*(function () {
-let x = new XMLHttpRequest();
-x.onreadystatechange = function () {
-  if (this.readyState == 4) {
-    if (this.status == 200) {
-      document.getElementsByTagName("header")[0].innerHTML = this.responseText;
-    }
-    if (this.status == 404) {
-      document.getElementsByTagName("header")[0].innerText =
-        "Navbar failed to load";
-    }
-  }
-};
-x.open("GET", "/sitio-personal/scripts/navbar.html", true);
-x.send();
-})()*/
-
 $.ajax({
   url: "/sitio-personal/scripts/navbar.html",
   context: $("header")[0]
